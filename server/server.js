af576c518e5b8ecce0a18961bb845bb50176c3b2
Ozafa <oz@d142-058-061-164.wireless.sfu.ca>
2024-11-16 15:53:42 -0800

    Changed default port to 5001

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/db');
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 
 // Connect to MongoDB
@@ -9,11 +9,11 @@ connectDB();
 app.use(express.json());
 
 // Define Routes
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/doctors', require('./routes/doctorRoutes'));
-app.use('/api/patients', require('./routes/patientRoutes'));
-app.use('/api/appointments', require('./routes/appointmentroutes'));
-app.use('/api/orders', require('./routes/orderRoutes'));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/doctors", require("./routes/doctorRoutes"));
+app.use("/api/patients", require("./routes/patientRoutes"));
+app.use("/api/appointments", require("./routes/appointmentroutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
