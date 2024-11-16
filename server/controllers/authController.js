e542fe645c6b8664eee5d00122044c974910aa56
Raghav Ahuja <raghavahuja2801@gmail.com>
2024-11-16 15:17:09 -0800

    added nodemon and /getme endpoint

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -29,3 +29,15 @@ exports.login = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+exports.getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password'); // Exclude the password
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
