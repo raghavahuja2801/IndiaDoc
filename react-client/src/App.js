5e3e54be3e6c4a8f9e318961ae1a25a100109eb6
Raghav Ahuja <raghavahuja@d207-023-165-170.wireless.sfu.ca>
2024-11-17 19:18:15 -0800

    fixed merges

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -4,15 +4,8 @@ import LandingPage from "./components/LandingPage"
 import LoginSignup from "./components/Auth";
 import HomePage from "./components/Dashboard";
 import HowItWorks from "./components/HowItWorks";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import Services from "./components/Services";
-=======
 import DoctorOnboarding from "./components/Doctor-Onboarding";
->>>>>>> Stashed changes
-=======
-import DoctorOnboarding from "./components/Doctor-Onboarding";
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -22,15 +15,12 @@ function App() {
         <Route path="/auth" element={<LoginSignup />} />
         <Route path="/dashboard" element={<HomePage />} />
         <Route path="/how-it-works" element={<HowItWorks />} />
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
         <Route path="/services" element={<Services />} />
-=======
+
         <Route path="/doctor-onboard" element={<DoctorOnboarding />} />
->>>>>>> Stashed changes
-=======
+
         <Route path="/doctor-onboard" element={<DoctorOnboarding />} />
->>>>>>> Stashed changes
       </Routes>
     </Router>
   );
